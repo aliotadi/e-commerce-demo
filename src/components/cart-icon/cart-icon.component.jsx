b09b5c7cee144ea1ad12df2e5e2801b20546f21c
ali otadi <ali.78.otadi@gmail.com>
2019-11-04 05:27:28 +0330

    add reselect ro project and reaplace cart selectors with memorized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,18 +4,23 @@ import { connect } from 'react-redux';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { createCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+
+const mapStateToProps = state => ({
+  itemCount: createCartItemsCount(state),
+});
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(CartIcon);
